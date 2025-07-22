0a1f78d8c210b45f9c9293fd3239f5160c71fcb7
MihretDe <mihretdesalegn98@gmail.com>
2025-07-22 19:13:53 +0300

    fix checker issue

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout' ),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
