6ed5beb3fa2f7632fcfa7be3cf71d4e5c378d4c7
MihretDe <mihretdesalegn98@gmail.com>
2025-07-22 17:17:35 +0300

    fix checker issue

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from urllib3 import HTTPResponse
 from .models import Book , Library
 from django.views.generic import DetailView
-# Create your views here.
+
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    output = '\n'.join([f"{book.title} by {book.author.name}" for book in books])
-    return HTTPResponse(output, content_type="text/plain")
+    books = Book.objects.all()  
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 
 class LibraryDetailView(DetailView):
