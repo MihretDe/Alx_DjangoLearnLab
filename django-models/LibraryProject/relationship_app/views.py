bb3af98895e65ea5d44a9072b09df6b37512f7bf
MihretDe <mihretdesalegn98@gmail.com>
2025-07-23 08:55:14 +0300

    fix checker issue

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test , login_required
 
 def list_books(request):
     books = Book.objects.all()  
@@ -35,22 +35,28 @@ def register(request):
     return render(request, 'relationship_app/register.html', {'form': form})
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
+#Checks if user is Librarian
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Librarian'
 
+@login_required
 @user_passes_test(is_librarian)
+
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
+#Checks if user is a Member
 def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
+@login_required
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
