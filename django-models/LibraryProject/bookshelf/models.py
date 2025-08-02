9958333c0494f31e90385a5f2699c21db1de5691
MihretDe <mihretdesalegn98@gmail.com>
2025-08-02 08:36:19 +0300

    fix checker issue

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser , BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+# Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+# Custom User Manager
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
@@ -16,17 +17,24 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def create_superuser(self, email, password=None, **extra_fields):
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
 
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+# Custom User Model
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     objects = CustomUserManager()
+
     def __str__(self):
         return self.email
