139de810613c58375c6572e938e55e3bc2530921
MihretDe <mihretdesalegn98@gmail.com>
2025-08-03 16:51:27 +0300

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-waj5wao%wykrp25m_58v%nc5dp5j!#)q=2o-*xg0otwf7((b6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,20 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+SECURE_BROWSER_XSS_FILTER = True
+
+X_FRAME_OPTIONS = 'DENY' 
+SECURE_CONTENT_TYPE_NOSNIFF = True 
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True 
+
+SECURE_SSL_REDIRECT = True 
+
+SECURE_HSTS_SECONDS = 31536000 
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True 
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') 
